0742b93a61fbcac3c4c10efe6c9612623fc7e990
Nidhin P Pramod <kichum386@gamil.com>
2023-04-13 15:18:20 +0530

    Added Search filter and corrected VideoBar

--- src/Context/UserContext.js
+++ src/Context/UserContext.js
@@ -4,7 +4,9 @@ import { db } from "../firebase.js";
 
 const userContext = createContext({
   fetchDetails: () => Function,
+  fetchVideoUrl: () => Function,
   data:null,
+  Videodata:null,
 });
 
 export const useUser = () => useContext(userContext);
@@ -12,16 +14,23 @@ export const useUser = () => useContext(userContext);
 export default function UserContextProvider({ children }) {
 
 const data=[]
+const Videodata=[]
  
 
  async function fetchDetails(collectionRef) {
     const docSnap=await getDocs(collection(db,"users"))
     docSnap.forEach(doc => {
-        console.log("Clicked");
         data.push(doc.data().displayName)
     })
   }
 
-  const value = { fetchDetails,data };
+ async function fetchVideoUrl(collectionRef) {
+    const docSnap=await getDocs(collection(db,"articles"))
+    docSnap.forEach(doc => {
+        Videodata.push(doc.data().actor.video)
+    })
+  }
+
+  const value = { fetchDetails,data,fetchVideoUrl,Videodata };
   return <userContext.Provider value={value}>{children}</userContext.Provider>;
 }
