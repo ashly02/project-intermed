2fcceaa65efb9f168e6395846ac4a97fc64deb5d
ashly02 <ashlyelias24@gmail.com>
2023-05-29 12:53:18 +0530

    firebase api change

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,12 @@ import 'firebase/compat/firestore';
 import 'firebase/compat/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCK1oFh9sHuybnSLQidVsZYByoZ_lc8HXY",
-  authDomain: "cusat-37be2.firebaseapp.com",
-  projectId: "cusat-37be2",
-  storageBucket: "cusat-37be2.appspot.com",
-  messagingSenderId: "531409539038",
-  appId: "1:531409539038:web:59c8f0bf491eba0ba4d74f"
+  apiKey: "AIzaSyDoWdOBJjYoejNoTNHqHdEgFFcDqC8tirc",
+  authDomain: "cusat-jn.firebaseapp.com",
+  projectId: "cusat-jn",
+  storageBucket: "cusat-jn.appspot.com",
+  messagingSenderId: "31443014638",
+  appId: "1:31443014638:web:d09f3b1997160f26f674ff"
 };
 firebase.initializeApp(firebaseConfig);
 
