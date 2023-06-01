3284f2d7768321acd5905f4c5f87c2b248568be5
feda <fedasalim567@gmail.com>
2023-06-01 21:07:51 +0530

    password visibility

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,19 +1,23 @@
-import React, { useState, useEffect } from 'react'
-import Add from "../../img/addpic.png"
+import React, { useState } from 'react';
+import Add from "../../img/addpic.png";
 import { createUserWithEmailAndPassword, updateProfile, sendEmailVerification } from "firebase/auth";
-import { auth, db, storage } from "../../firebase"
+import { auth, db, storage } from "../../firebase";
 import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
-import { doc, setDoc } from "firebase/firestore"; 
+import { doc, setDoc } from "firebase/firestore";
 import { useNavigate, Link } from 'react-router-dom';
 import logo from './logo.png';
-import './Signup.css'
-const Signup = () => {
-  const [err, setErr] = useState(false)
-  const [isEmailSent, setIsEmailSent] = useState(false)
-  const navigate = useNavigate();
+import './Signup.css';
+import showImage from "./showpassword.svg" ;
+import hideImage from "./hidepassword.svg" ;
 
+const Signup = () => {
+  const [err, setErr] = useState(false);
+  const [isEmailSent, setIsEmailSent] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+  const navigate = useNavigate();
+  
   const handleSubmit = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
     const displayName = e.target[0].value;
     const email = e.target[1].value;
     const password = e.target[2].value;
@@ -27,10 +31,10 @@ const Signup = () => {
     }
     try {
       const res = await createUserWithEmailAndPassword(auth, email, password);
-    
+
       const storageRef = ref(storage, displayName);
       const uploadTask = uploadBytesResumable(storageRef, file);
-    
+
       uploadTask.on(
         (error) => {
           alert('Error uploading avatar. Please try again.')
@@ -43,21 +47,21 @@ const Signup = () => {
                 displayName,
                 photoURL: downloadURL,
               });
-    
+
               await setDoc(doc(db, "users", res.user.uid), {
                 uid: res.user.uid,
                 displayName,
                 email,
                 photoURL: downloadURL,
               });
-    
+
               await setDoc(doc(db, "userChats", res.user.uid), {});
-    
+
               await sendEmailVerification(res.user); // send email verification
-    
+
               setIsEmailSent(true); // set state to indicate that email was sent
-              alert('Please verify your email and refresh the page.')
-    
+              alert('Please verify your email and refresh the page.');
+
             } catch (err) {
               alert('Error creating user. Please try again.')
               console.log(err);
@@ -76,46 +80,51 @@ const Signup = () => {
       }
     }
   };
-  useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged((user) => {
-      if (user) {
-        if (user.emailVerified) {
-          navigate("/home");
-        }
-      }
-      
-    });
-    return unsubscribe;
-  }, [navigate]);
+
+  const toggleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
 
   return (
     <div className='main-signup'>
       <div className='signup-container'>
         <div className='left-side'>
-        <div className='welcome'>WELCOME ABOARD!</div>
-      <img src={logo} className="applogo" alt="logo"  />
-        <span className='logo'>engage,inspire,connect..</span>
+          <div className='welcome'>WELCOME ABOARD!</div>
+          <img src={logo} className="applogo" alt="logo" />
+          <span className='logo'>engage,inspire,connect..</span>
         </div>
         <div className='right-side'>
-        <div className='title'>SIGN UP</div>
-        <form onSubmit={handleSubmit}>
-          <input type="text" placeholder='User name'></input> 
-          <input type="email" placeholder='Email'></input>
-          <input type="password" placeholder='Password' />
-          <input style={{ display: "none" }} type="file" id='file' />
-          <label htmlFor="file">
-            <img src={Add} alt="" />
-            <div className='upload'>Upload Profile Picture</div>
-          </label>
-          <button>Sign up</button>
-          {err}
-          {isEmailSent && <span></span>}
-          <div className='para'>Already have an account? <Link to="/">Login</Link> </div>
-        </form>
+          <div className='title'>SIGN UP</div>
+          <form onSubmit={handleSubmit}>
+            <input type="text" placeholder='User name'></input>
+            <input type="email" placeholder='Email'></input>
+            <div className="password-input-container">
+              <input
+                type={showPassword ? "text" : "password"}
+                placeholder='Password'
+                className={`password-input ${showPassword ? "show-password" : ""}`}
+              />
+              <img
+                src={showPassword ? hideImage : showImage}
+                alt="Toggle Password Visibility"
+                className="password-toggle-image"
+                onClick={toggleShowPassword}
+              />
+            </div>
+            <input style={{ display: "none" }} type="file" id='file' />
+            <label htmlFor="file">
+              <img src={Add} alt="" />
+              <div className='upload'>Upload Profile Picture</div>
+            </label>
+            <button>Sign up</button>
+            {err}
+            {isEmailSent && <span></span>}
+            <div className='para'>Already have an account? <Link to="/">Login</Link> </div>
+          </form>
         </div>
       </div>
     </div>
   )
 }
 
-export default Signup
+export default Signup;
